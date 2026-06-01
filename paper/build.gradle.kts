80d8e0568f91fb67e233b8c51df4c44c2f61873b
lunarenzo <lunarenzo@users.noreply.github.com>
2026-06-02 01:25:22 +0800

    fix: explicitly add net.kyori:adventure-platform-bukkit dependency

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -20,6 +20,14 @@ dependencies {
         exclude(group = "net.kyori", module = "adventure-text-serializer-plain")
         exclude(group = "net.kyori", module = "adventure-text-serializer-json")
     }
+    implementation("net.kyori:adventure-platform-bukkit:4.3.3") {
+        exclude(group = "net.kyori", module = "adventure-api")
+        exclude(group = "net.kyori", module = "adventure-text-serializer-gson")
+        exclude(group = "net.kyori", module = "adventure-text-serializer-legacy")
+        exclude(group = "net.kyori", module = "adventure-text-serializer-plain")
+        exclude(group = "net.kyori", module = "adventure-text-serializer-json")
+        exclude(group = "net.kyori", module = "adventure-text-serializer-bungeecord")
+    }
     api(libs.colorparser.paper) {
         exclude("net.kyori")
     }
