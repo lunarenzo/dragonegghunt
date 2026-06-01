def1d0177867c5759c5a2d7b6a2f1162d28aa315
lunarenzo <lunarenzo@users.noreply.github.com>
2026-06-02 02:29:17 +0800

    fix: import org.bukkit.event.inventory.InventoryCloseEvent to resolve compile error

--- paper/src/main/java/com/lunatech/dragonegghunt/listener/EggMovementListener.java
+++ paper/src/main/java/com/lunatech/dragonegghunt/listener/EggMovementListener.java
@@ -28,6 +28,7 @@ import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
